6f4b44dc805301a649b7d68dc8dd1db6a6cd4ce9
Joshua Mabawonku <drakejoshua086@gmail.com>
2024-07-21 07:01:23 +0100

    mark: Added app's styling and layout
    - Added the app's styling and layout such as:
      the board, reset-buttons etc
    - preparing the app for scripting

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,91 @@ function App() {
       {/* app's board component */}
       <div className="board">
         {/* first row of boxes on board */}
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
+        <div className="box hover-x hover-o select-x select-o">
+          <span className="material-symbols-outlined icon x">
+            close
+          </span>
+
+          <span className="material-symbols-outlined icon o">
+            radio_button_unchecked
+          </span>
+        </div>
+        <div className="box hover-x hover-o select-x select-o">
+          <span className="material-symbols-outlined icon x">
+            close
+          </span>
+
+          <span className="material-symbols-outlined icon o">
+            radio_button_unchecked
+          </span>
+        </div>
+        <div className="box hover-x hover-o select-x select-o">
+          <span className="material-symbols-outlined icon x">
+            close
+          </span>
+
+          <span className="material-symbols-outlined icon o">
+            radio_button_unchecked
+          </span>
+        </div>
 
         {/* second row of boxes on board */}
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
+        <div className="box hover-x hover-o select-x select-o">
+          <span className="material-symbols-outlined icon x">
+            close
+          </span>
+
+          <span className="material-symbols-outlined icon o">
+            radio_button_unchecked
+          </span>
+        </div>
+        <div className="box hover-x hover-o select-x select-o">
+          <span className="material-symbols-outlined icon x">
+            close
+          </span>
+
+          <span className="material-symbols-outlined icon o">
+            radio_button_unchecked
+          </span>
+        </div>
+        <div className="box hover-x hover-o select-x select-o">
+          <span className="material-symbols-outlined icon x">
+            close
+          </span>
+
+          <span className="material-symbols-outlined icon o">
+            radio_button_unchecked
+          </span>
+        </div>
 
         {/* third row of boxes on board */}
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
+        <div className="box hover-x hover-o select-x select-o">
+          <span className="material-symbols-outlined icon x">
+            close
+          </span>
+
+          <span className="material-symbols-outlined icon o">
+            radio_button_unchecked
+          </span>
+        </div>
+        <div className="box hover-x hover-o select-x select-o">
+          <span className="material-symbols-outlined icon x">
+            close
+          </span>
+
+          <span className="material-symbols-outlined icon o">
+            radio_button_unchecked
+          </span>
+        </div>
+        <div className="box hover-x hover-o select-x select-o">
+          <span className="material-symbols-outlined icon x">
+            close
+          </span>
+
+          <span className="material-symbols-outlined icon o">
+            radio_button_unchecked
+          </span>
+        </div>
       </div>
 
       {/* board reset button */}
