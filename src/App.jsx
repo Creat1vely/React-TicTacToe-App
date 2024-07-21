2df9f2f67d5eb067b025d3f9674c2fc493100c6b
Joshua Mabawonku <drakejoshua086@gmail.com>
2024-07-21 06:35:49 +0100

    mark: Added app's markup/jsx
    - added the markup/jsx needed to structure/
      layout the app
    - preparing the app for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 function App() {
   return (
     <div className='app'>
-      
+      {/* app's title */}
+      <h1 className="app-title">tic tac toe</h1>
+
+      {/* app's board component */}
+      <div className="board">
+        {/* first row of boxes on board */}
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+
+        {/* second row of boxes on board */}
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+
+        {/* third row of boxes on board */}
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+
+      {/* board reset button */}
+      <button className="reset-btn">
+        reset
+      </button>
     </div>
   )
 }
